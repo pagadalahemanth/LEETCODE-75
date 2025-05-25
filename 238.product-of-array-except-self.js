b6a449e06f71174e4d7fc2a56fbbbfdf1eeb6b98
pagadalahemanth07@gmail.com <pagadalahemanth07@gmail.com>
2025-05-25 16:20:53 +0530

    Alter for product of rray except self

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -51,17 +51,33 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var productExceptSelf = function(nums) {
+//     let n = nums.length;
+//     let result=[];
+//     for (let i=0;i<n;i++){
+//         let product=1;
+//         for (let j=0;j<n;j++){
+//         if(i!==j){
+//             product *= nums[j];
+//         }
+//         }
+//     result.push(product);
+//     }
+//     return result;
+// };
+
 var productExceptSelf = function(nums) {
     let n = nums.length;
-    let result=[];
-    for (let i=0;i<n;i++){
-        let product=1;
-        for (let j=0;j<n;j++){
-        if(i!==j){
-            product *= nums[j];
-        }
-        }
-    result.push(product);
+    let result = new Array(n).fill(1);
+    let leftProduct = 1;
+    for (let i = 0; i < n; i++) {
+        result[i] *= leftProduct;
+        leftProduct *= nums[i];
+    }
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] *= rightProduct;
+        rightProduct *= nums[i];
     }
     return result;
 };
